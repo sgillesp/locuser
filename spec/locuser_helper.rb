504fdf4c8918053a9285986483722801e464b19a
hgillesp <masterofratios@gmail.com>
2015-08-24 14:17:40 -0700

    use default address formatting, by default

--- spec/locuser_helper.rb
+++ spec/locuser_helper.rb
@@ -4,8 +4,7 @@ require 'locuser/configuration'
 
 # make sure this comes before requiring these files
 Locuser.configure do |c|
-    c.use_geocoder = false
-    c.parser_class = ::StreetAddress::US  # this uses the StreetAddress gem
+   c.use_geocoder = false
 end
 
 Dir[File.join(ENGINE_RAILS_ROOT, 'lib/**/*.rb')].each {|f| require f}
