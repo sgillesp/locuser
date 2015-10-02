8e5b82067fed617486b519a0ed5f2f439b0bd542
hgillesp <masterofratios@gmail.com>
2015-10-01 21:36:26 -0700

    update for taxonomite - does not use name anymore

--- spec/models/locuser/specbase.rb
+++ spec/models/locuser/specbase.rb
@@ -6,9 +6,10 @@ require 'active_support'
 module Locuser
   module SpecBase
     extend ActiveSupport::Concern
-    
+
     included do
       field :description, type: String
+      field :name, type: String
 
     end
   end
